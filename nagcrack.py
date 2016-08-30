e18ee5d2a9c7b5d4b4123c46ba1051c340147592
Niklas Jarck <niklas@jarckn.de>
2016-08-30 10:23:56 +0200

    - Some PEP8 corrections. - Code optimization.

--- nagcrack.py
+++ nagcrack.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 
-import argparse, zlib, base64
+import argparse
+import zlib
+import base64
+import binascii
 
-class color:
+
+class Color:
     OK = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
@@ -13,24 +17,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--verbose", help="Show more output", action="store_true")
 parser.add_argument("string", help="String to be decoded")
 args = parser.parse_args()
+toencode = ''
 try:
-	if args.verbose: print("Decoding string for the first time... "),
-	toencode = base64.b64decode(args.string);
-	print(color.OK + "done." + color.ENDC)
-except:
-	print(color.FAIL + "error." + color.ENDC)
-try:
-	if args.verbose: print("Looping through decompressed stringlist... "),
-	for i in range(5):
-		toencode = zlib.decompress(toencode)
-		toencode = list(toencode)
-		toencode.reverse()
-		toencode = "".join(toencode)
-		toencode = base64.b64decode(toencode)
-	print(color.OK + "done." + color.ENDC)
-except:
-	print(color.FAIL + "error." + color.ENDC)
-print
-print("Decoded string: " + color.WARNING + toencode + color.ENDC)
-print
-	
+    if args.verbose: print("Decoding string for the first time... "),
+    toencode = base64.b64decode(args.string)
+    print(Color.OK + "done." + Color.ENDC)
+except binascii.Error:
+    print(Color.FAIL + "error.")
+    print("No correct base64-encoded string defined." + Color.ENDC)
+if args.verbose: print("Looping through decompressed stringlist... "),
+for i in range(5):
+    toencode = zlib.decompress(toencode)
+    toencode = list(toencode)
+    toencode.reverse()
+    toencode = "".join(toencode)
+    toencode = base64.b64decode(toencode)
+print(Color.OK + "done." + Color.ENDC)
+print("\nDecoded string: " + Color.WARNING + toencode + Color.ENDC + "\n")
